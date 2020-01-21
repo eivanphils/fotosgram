9878015610a258be5ccff630ae2d12173c8001df
Phils Garcia <eivanphils@gmail.com>
2020-01-21 15:06:50 -0300

    added pages, components, interfaces and service

---
+++ src/app/services/posts.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import {RespuestaPosts} from '../interfaces/interfaces';
+
+const URL = environment.url;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  pagePosts = 0;
+
+  constructor(private http: HttpClient) { }
+
+
+  getPosts() {
+    this.pagePosts++;
+
+    return this.http.get<RespuestaPosts>(`${URL}/posts/?pagina=${this.pagePosts}`);
+  }
+}
