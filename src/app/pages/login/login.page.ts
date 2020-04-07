2f6364020517d0fac504883896988575dbcabb16
Phils Garcia <eivanphils@gmail.com>
2020-04-07 09:09:00 -0400

    agregado diseño de vista de login

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  avatars = [
+    {
+      img: 'av-1.png',
+      seleccionado: true
+    },
+    {
+      img: 'av-2.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-3.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-4.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-5.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-6.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-7.png',
+      seleccionado: false
+    },
+    {
+      img: 'av-8.png',
+      seleccionado: false
+    },
+  ];
+
+  avatarOpts = {
+    slidesPerView: 3.5
+  };
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  login(loginForm: NgForm) {
+    console.log(loginForm.valid);
+    
+  }
+
+  register(registerForm: NgForm) {
+    console.log(registerForm.valid);
+    
+  }
+
+  selectAvatar(avatar) {
+    this.avatars.forEach( av => av.seleccionado = false);
+
+    avatar.seleccionado = true;
   }
 
 }
